856c6ca2788165a3802cfac507d6eb85547f361e
Atilio Pereira <apereira@consumeraffairs.com>
2023-11-09 17:04:37 -0300

    BE-1 Add Event admin

--- app/events/admin.py
+++ app/events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from app.events.models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "date", "location", "status")
+    list_filter = ("status",)
+    search_fields = (
+        "id",
+        "title",
+        "description",
+    )
+    fields = (
+        "title",
+        "description",
+        "date",
+        "location",
+        "status",
+    )
+    readonly_fields = ("id", "status", "created_at", "updated_at")
